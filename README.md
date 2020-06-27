a5e22fa2441f4c2fdca39c757766c01ebb4b8832
amarolahy <ramah.samuel@gmail.com>
2020-06-27 21:57:10 +0300

    Configure webpack for dev server and prod build

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Sass-Typescript Boilerplate
 WCC Front Challenge S6
 
+Martian Login Form
+
 ## Install
 `npm install`
 
-## Build for dev
-`npm run webpack:dev`
+## Run
+`npm run start`
+
+Open your browser at http://localhost:8080. 
+
+Enjoy it ! :D
+
+## Production build
+`npm run build`
--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,14 @@
+const path = require('path');
+
 module.exports = {
-  mode: "development",
-  watch: true,
   entry: "./src/ts/index.ts",
   output: {
     filename: "bundle.js",
-    path: __dirname + "/dist"
+    path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"]
   },
-  devtool: "source-map",
   module: {
     rules: [
       { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] },
